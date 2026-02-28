966188c24bcc0c222029e6c876da1ead3bf7a292
JaxL9 <Jax.bug.bug@gmail.com>
2026-02-28 07:29:37 -0600

    Update MuscleLegends.lua

--- MuscleLegends.lua
+++ MuscleLegends.lua
@@ -17,13 +17,13 @@ local ScriptsTab = Window:CreateTab("Scripts", 4483362458)
 local InfoTab = Window:CreateTab("Info", 4483362458)
 
 ---------------------------------------------------
--- SCRIPT BUTTONS (MERGED DIRECT LOAD)
+-- SAFE LOAD FUNCTION
 ---------------------------------------------------
 
 local function SafeLoad(url)
     Rayfield:Notify({
         Title = "Loading Script",
-        Content = url,
+        Content = "Please wait...",
         Duration = 3
     })
 
@@ -47,40 +47,42 @@ local function SafeLoad(url)
     end
 end
 
+---------------------------------------------------
+-- SCRIPT BUTTONS
+---------------------------------------------------
+
 ScriptsTab:CreateButton({
     Name = "Anti AFK",
     Callback = function()
-        SafeLoad("https://raw.githubusercontent.com/iblameaabis/Enchanted/refs/heads/main/Anti-AFK")
+        SafeLoad("https://raw.githubusercontent.com/iblameaabis/Enchanted/main/Anti-AFK")
     end
 })
 
 ScriptsTab:CreateButton({
     Name = "Muscle Legends (Server Hop Kill)",
     Callback = function()
-        SafeLoad("https://raw.githubusercontent.com/iblameaabis/Enchanted/refs/heads/main/ML%20Killing%20%5BServer%20Hop%5D")
+        SafeLoad("https://raw.githubusercontent.com/iblameaabis/Enchanted/main/ML%20Killing%20%5BServer%20Hop%5D")
     end
 })
 
 ScriptsTab:CreateButton({
-       Name = "Redz Hub"
-        Callback = function()
-            SafeLoad("https://raw.githubusercontent.com/jynzl/main/Musclas%20Legenos.lua"
+    Name = "Redz Hub",
+    Callback = function()
+        SafeLoad("https://raw.githubusercontent.com/jynzl/main/main/Musclas%20Legenos.lua")
     end
 })
 
-    
 ScriptsTab:CreateButton({
     Name = "Legends of Speed",
     Callback = function()
-        SafeLoad("https://raw.githubusercontent.com/iblameaabis/Enchanted/refs/heads/main/Legends%20Of%20Speed")
+        SafeLoad("https://raw.githubusercontent.com/iblameaabis/Enchanted/main/Legends%20Of%20Speed")
     end
 })
 
 ScriptsTab:CreateButton({
-    
     Name = "Ninja Legends",
     Callback = function()
-        SafeLoad("https://raw.githubusercontent.com/iblameaabis/Enchanted/refs/heads/main/Ninja%20Legends")
+        SafeLoad("https://raw.githubusercontent.com/iblameaabis/Enchanted/main/Ninja%20Legends")
     end
 })
 
